44544c34b78b3399ffa991c55cec10e5f39a8279
Kaushal Pareek <37943355+kp3393@users.noreply.github.com>
2022-08-09 18:15:48 +0200

    Added preprocessing of the images for C4W3A2_UNET.py file

--- Week 3/C4W3A2_UNET.py
+++ Week 3/C4W3A2_UNET.py
@@ -29,7 +29,8 @@ from keras.layers import concatenate
 from test_utils import summary, comparator
 
 
-"==========########## Load and Split the Data ##########=========="
+"==========########## Loading, associating and preprocessing the Data ##########=========="
+# load the dataset
 ROOT_DIR = os.getcwd()
 image_path = os.path.join(ROOT_DIR, 'Data', 'C4W3A2', 'CARLA', 'CameraRGB')
 mask_path = os.path.join(ROOT_DIR, 'Data', 'C4W3A2', 'CARLA', 'CameraSeg')
@@ -40,19 +41,60 @@ image_list = [os.path.join(image_path, i) for i in image_list]
 mask_list = [os.path.join(mask_path, i) for i in mask_list]
 
 # Investigating images
-img_nr = 5
+img_nr = 0
 img = imageio.imread(image_list[img_nr])
 print('Image dimensions are ', img.shape)
 
 mask = imageio.imread(mask_list[img_nr])
 print('Mask dimensions are ', mask.shape)
-print(np.unique(mask[:, :, 0]))
-print(np.unique(mask[:, :, 1]))
-print(np.unique(mask[:, :, 2]))
+print('%i classes in the masked image' % np.unique(mask[:, :, 0]).shape[0])
 
-fig, ax = plt.subplots(1, 2)
-ax[0].imshow(img)
-ax[0].set_title('Image')
-ax[1].imshow(mask[:, :, 0])
-ax[1].set_title('Segmentation')
-plt.show()
+# look at the image and corresponding mask
+# fig, ax = plt.subplots(1, 2)
+# ax[0].imshow(img)
+# ax[0].set_title('Image')
+# ax[1].imshow(mask[:, :, 0])
+# ax[1].set_title('Segmentation')
+# plt.show()
+
+# associate each image with its corresponding mask and create a tf.data.dataset object
+dataset = tf.data.Dataset.from_tensor_slices((image_list, mask_list))
+
+
+def process_path(image_path, mask_path):
+    """
+    Creating a tensor out of the image and the mask
+    :param image_path: string, path of the image
+    :param mask_path: string, path of the mask
+    :return:
+    img: tensor of dtype tf.float32
+    mask: tensor of unint8
+    """
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_png(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=3)
+    mask = tf.math.reduce_max(mask, axis=-1, keepdims=True)
+    return img, mask
+
+
+def preprocess(image, mask):
+    """
+    Resizes the image and the mask to (96, 128). Also, normalizes the image.
+    :param image: 3D tensor, output of process_path
+    :param mask: 3D tensor, output of the process_path
+    :return:
+    input_image: resize and normalized input image
+    input_mask: resize input mask
+    """
+    input_image = tf.image.resize(image, (96, 128), method='nearest')
+    input_mask = tf.image.resize(mask, (96, 128), method='nearest')
+
+    input_image = input_image / 255.
+
+    return input_image, input_mask
+
+image_ds = dataset.map(process_path)
+processed_image_ds = image_ds.map(preprocess)
