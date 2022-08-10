9bea019042e6ba54bd3d6cfdd3833b4dd9973088
Kaushal Pareek <37943355+kp3393@users.noreply.github.com>
2022-08-10 17:48:46 +0200

    Created the Unet model. Updates in README.md.

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-# coursera_DLS_C4_CNN
-Coursera deep learning specialization (CNN) assignments. 
-As access to assignments is lost after finishing the course, this repository will help to go through the assignments on your IDE. 
-The dataset for each assignment is in its respective folder. Here are the links to the assignment, just in case one needs to download them separately.
+# Coursera Deep Learning Specialization Class 4 (CNN) using TensorFlow 2.x
+As access to assignments is lost after finishing the course, this repository will help to go through the assignments on IDE. The aim is to help one brush up on the concepts taught by Professor Andrew Ng.
+
+Summary of each assignment for quick reference:
+
+# C4W1 Assignments
+
+AIM: Functional and sequential implementation of the TENSORFLOW models.
+
+Number of assignments: 2 (Convolution_model_Step_by_Step, Convolution_model_Application). Only the second assignment (Convolution_model_Application) is TensorFlow based and is included in the repository. 
+
+Link to datasets: **[Happy Face](https://www.kaggle.com/datasets/iarunava/happy-house-dataset)**, **[hand signs](https://github.com/kp3393/coursera-deep-learning-specialization/tree/master/C4%20-%20Convolutional%20Neural%20Networks/Week%201/datasets)**
+
+Assignment 1: Binary Classification on **[Happy Face](https://www.kaggle.com/datasets/iarunava/happy-house-dataset)** dataset. 
+
+- Build and train a ConvNet in TF for a binary classification problem using **sequential API.**
+- Dataset: The data is stored in **[h5 format](https://docs.h5py.org/en/stable/).** More details can be found [here](https://docs.h5py.org/en/stable/quick.html).
+    - Train and Test data is provided.
+    - Training dataset has 600 images and images are of shape (64, 64, 3). The batch can be represented as (600, 64, 64, 3).
+    - Test dataset has 150 images with same dimensions as train.
+
+Assignment 2: Multiclass Classification on  **[hand signs](https://github.com/kp3393/coursera-deep-learning-specialization/tree/master/C4%20-%20Convolutional%20Neural%20Networks/Week%201/datasets)** dataset.
+
+- Build and train a ConvNet in TF for a multiclass classification using the Functional API.
+- Dataset: The data is stored in h5 format like assignment 1.
+- Main difference is the use of more flexible functional API and use of one hot encoding.
+
+# C4W2 Assignments
+
+AIM: ResNet model and MobileNet model implementation.
+
+Number of assignments: 2 (Residual Network and MobileNet) 
+
+Link to datasets: **[hand signs](https://github.com/kp3393/coursera-deep-learning-specialization/tree/master/C4%20-%20Convolutional%20Neural%20Networks/Week%201/datasets)**, [Alpaca/Not Alpaca](https://www.kaggle.com/datasets/sid4sal/alpaca-dataset-small)
+
+Assignment 1: ResNet50
+
+- Implement the basic building blocks of ResNets in a deep neural network using Keras.
+- Put building blocks (identity block and convolution block) to implement and train a state-of-the-art neural network for image classification.
+- Implement a skip connection in your network.
+- Dataset: hand signs dataset stored in h5 format. Same as C4W1A1.
+
+Assignment 2: MobileNet
+
+- Create a dataset from a directory.
+- Preprocess and augment data using the Sequential API.
+- Adapt a pretrained model to new data and train a classifier using the Functional API and MobileNet.
+- Fine-tune a classifier's final layers to improve accuracy.
+- Fine-tune the final layers of your model to capture high-level details near the end of the network and potentially improve accuracy
+- Dataset: The model was trained on [Alpaca/Not Alpaca](https://www.kaggle.com/datasets/sid4sal/alpaca-dataset-small) dataset.
+    - The images were stored in two folders names as ‘Alpaca’ and ‘Not Alpaca’. The name of these two filters becomes two classes which are to be classified.
--- Week 3/C4W3A2_UNET.py
+++ Week 3/C4W3A2_UNET.py
@@ -98,3 +98,165 @@ def preprocess(image, mask):
 
 image_ds = dataset.map(process_path)
 processed_image_ds = image_ds.map(preprocess)
+
+
+def display(display_list):
+    """
+    Displays an input image, the ground truth and the predicted mask
+    :param display_list: list, list of images to be displayed. The expected order is: input image, ground truth
+                        and predicted image
+    :return:
+    Displays the images in a 1x3 format
+    """
+    plt.figure(figsize=(15, 15))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+
+
+# plotting the images from the dataset without pre-processing
+for image, mask in image_ds.take(1):
+    print(image.shape)
+    display([image, mask])
+
+for image, mask in processed_image_ds.take(1):
+    print(image.shape)
+    display([image, mask])
+
+
+
+"==========########## UNET MODEL ##########=========="
+
+
+def conv_block(inputs=None, n_filters=32, dropout_prob=0, max_pooling=True):
+    """
+    Convolution block for Unet encoder.
+    These are the steps:
+    1. Adds 2 Conv2D layers with n_filters filters with kernel_size set to 3,
+       kernel_initializer set to 'he_normal', padding set to 'same' and 'relu' activation.
+    2. if dropout_prob > 0, then adds a Dropout layer with parameter dropout_prob
+    3. if max_pooling is set to True, then adds a MaxPooling2D layer with 2x2 pool size
+    :param inputs: tensor, input tensor
+    :param n_filters: int, number of filters for the convolution layers
+    :param dropout_prob: float, dropout probability
+    :param max_pooling: bool, maxpooling to reduce the spatial dimension of the output volume
+    :return:
+    next_layer, skip_connection: Next layer and skip connection outputs
+    """
+    # step 1: Two conv2D layers
+    conv = Conv2D(n_filters, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv = Conv2D(n_filters, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv)
+    # step 2: Check for the dropout_prob
+    if dropout_prob > 0:
+        conv = Dropout(dropout_prob)(conv)
+    # Check if maxpooling needs to be done before passing to next_layer
+    if max_pooling:
+        next_layer = MaxPooling2D(2, strides=2)(conv)
+    else:
+        next_layer = conv
+    # get the skip connection layer
+    skip_layer = conv
+
+    return next_layer, skip_layer
+
+
+def upsampling_block(expansive_input, contractive_input, n_filters=13):
+    """
+    Convolutional upsampling block
+    These are the steps:
+    1. Perform up sampling (Conv2DTranspose) on the previous layer input (expansive_input)
+    2. Merge the upsampled expansive_input with skip_connection input (contractive_input)
+    3. Perform 2 Conv2D on the merged output
+    :param expansive_input: tensor, input tensor from previous layer
+    :param contractive_input: tensor, skip connection
+    :param n_filters: int, number of filters from the convolutional layers
+    :return:
+    conv: Tensor output
+    """
+    # step 1: perform transpose convolution on the expansive input (i.e. input from the previous layer)
+    up = Conv2DTranspose(n_filters,
+                         3,
+                         strides=2,
+                         padding='same')(expansive_input)
+    # step 2: merge it with the skip connection input
+    merge = concatenate([up, contractive_input], axis=3)
+    # step 3: perform convolutions (2 Conv2D) on the merged dataset
+    conv = Conv2D(n_filters, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge)
+    conv = Conv2D(n_filters, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv)
+
+    return conv
+
+
+def unet_model(input_size=(96, 128, 3), n_filters=32, n_classes=13):
+    """
+    The final unet model
+    :param input_size: tuple, input shape
+    :param n_filters: int, number of filters for the convolution layers
+    :param n_classes: number of output classes
+    :return:
+    model: tf.keras.Model
+    """
+    # step1: get the input
+    inputs = Input(input_size)
+
+    # Step2: Begining of the contracting path/ encoder block
+    cblock1 = conv_block(inputs=inputs, n_filters=n_filters)
+    # chaining the next_layer output of first conv cblock1 with the input of the next block.
+    # Also, double the number of filters from here on
+    cblock2 = conv_block(inputs=cblock1[0], n_filters=n_filters*2)
+    cblock3 = conv_block(inputs=cblock2[0], n_filters=n_filters*4)
+    # for cblock4, apply dropout of 0.3
+    cblock4 = conv_block(inputs=cblock3[0], n_filters=n_filters*8, dropout_prob=0.3)
+    # for cblock5, have dropout of 0.3 with no max_pooling
+    cblock5 = conv_block(inputs=cblock4[0], n_filters=n_filters*16, dropout_prob=0.3, max_pooling=False)
+
+    # Step3: Begining of the expanding path/ decoder path
+    # ublock6 --> expansive_block=cblock5[0], contractive_block=cblock4[1], reduce the n_filter by half
+    ublock6 = upsampling_block(cblock5[0], cblock4[1], n_filters*8)
+    # ublock7 --> expansive_block=ublock6, contractive_block=cblock3[1], reduce the n_filter by half
+    ublock7 = upsampling_block(ublock6, cblock3[1], n_filters*4)
+    # ublock8 --> expansive_block=ublock7, contractive_block=cblock2[1], reduce the n_filter by half
+    ublock8 = upsampling_block(ublock7, cblock2[1], n_filters*2)
+    # ublock9 --> upsampling_block=ublock8, contractive_block=cblock1[1], reduce the n_filter by half
+    ublock9 = upsampling_block(ublock8, cblock1[1], n_filters)
+
+    conv9 = Conv2D(n_filters, 3, activation='relu', padding='same', kernel_initializer='he_normal')(ublock9)
+    conv10 = Conv2D(n_classes, 1, padding='same')(conv9)
+
+    model = keras.Model(inputs=inputs, outputs=conv10)
+
+    return model
+
+
+"==========########## UNET model training ##########=========="
+
+# Set model parameters and looking at the summary
+img_height = 96
+img_width = 128
+num_channels = 3
+filters = 32
+n_classes = 13
+
+unet = unet_model((img_height, img_width, num_channels), n_filters=filters, n_classes=n_classes)
+unet.summary()
+
+# compiling the model
+unet.compile(optimizer='adam',
+             loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+             metrics=['Accuracy'])
+
+# traning the model
+EPOCHS = 40
+VAL_SUBSPLITS = 5
+BUFFER_SIZE = 500
+BATCH_SIZE = 32
+processed_image_ds.batch(BATCH_SIZE)
+train_dataset = processed_image_ds.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+print(processed_image_ds.element_spec)
+model_history = unet.fit(train_dataset, epochs=EPOCHS)
